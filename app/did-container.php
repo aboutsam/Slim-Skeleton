6d62041993569d4007f5c58c9744119bd39c0d22
Gavrila Florin <gavrilacflorin@yahoo.com>
2020-02-04 00:52:23 +0200

    extract did container to separate file

---
+++ app/did-container.php
@@ -0,0 +1,24 @@
+<?php
+use DI\ContainerBuilder;
+
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+
+if (false) { // Should be set to true in production
+    $containerBuilder->enableCompilation(__DIR__ . '/../var/cache');
+}
+
+// Set up settings
+$settings = require __DIR__ . '/settings.php';
+$settings($containerBuilder);
+
+// Set up dependencies
+$dependencies = require __DIR__ . '/dependencies.php';
+$dependencies($containerBuilder);
+
+// Set up repositories
+$repositories = require __DIR__ . '/repositories.php';
+$repositories($containerBuilder);
+
+// Build PHP-DI Container instance
+return $containerBuilder->build();
